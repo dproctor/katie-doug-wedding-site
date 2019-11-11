1a90a231a2fecca02ff631b86a0ac4b4a73fa69d
Devon Proctor <devon.proctor@gmail.com>
2019-11-11 13:00:29 -0800

    clean up photo rendering

--- assets/js/wedding.js
+++ assets/js/wedding.js
@@ -16,12 +16,20 @@ function markFormAsSubmitted() {
   form.classList.add('submitted');
 }
 
+function makePhotosVisible() {
+  var photos = document.querySelectorAll("#photo-container img");
+  for (var i=0; i<photos.length; i++) {
+    photos[i].style["opacity"] = 1;
+  }
+}
+
 window.addEventListener('load', function() {
   var menuButton = document.getElementById('mobile-nav-container');
   menuButton.onclick = function() {
     menuButton.classList.toggle('menu-active');
   };
   repositionRsvpButton();
+  makePhotosVisible();
 });
 
 window.addEventListener('resize', function() {
