8f2b3072e5995a284dbb93bcf090f80dec6e2237
Devon Proctor <devon.proctor@gmail.com>
2019-12-04 20:51:37 -0800

    js repositioning was flaky. use css media queries instead

--- assets/js/wedding.js
+++ assets/js/wedding.js
@@ -1,13 +1,3 @@
-
-function repositionRsvpButton() {
-  var afterPhoto = document.getElementsByClassName('after-photo');
-  var photo = document.getElementsByClassName('photo');
-
-  if (afterPhoto.length && photo.length) {
-    afterPhoto[0].style.marginTop = (photo[0].height + 60) + 'px';
-  }
-}
-
 function markFormAsSubmitted() {
   var form = document.getElementById('rsvp-form');
   if (!form) {
@@ -23,15 +13,11 @@ function makePhotosVisible() {
   }
 }
 
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('load', function() {
   var menuButton = document.getElementById('mobile-nav-container');
   menuButton.onclick = function() {
     menuButton.classList.toggle('menu-active');
   };
-  repositionRsvpButton();
-});
-
-window.addEventListener('load', function() {
   makePhotosVisible();
 });
 
